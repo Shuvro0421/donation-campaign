81e131bbefa9a48800fa0e121b93f022a9e5da69
Shuvro0421 <adibarmanshuvro89@gmail.com>
2023-09-25 12:41:45 +0600

    error page done properly

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
+
+
 const Error = () => {
     return (
         <div>
-            <h1 className="text-4xl text-center text-red-500 font-bold">404 Page not found!</h1>
+            <h1 className="md:text-4xl text:3xl flex flex-col gap-5 text-center my-60 text-red-500 font-bold">404 Page not found! <span className="text-lg"><Link to="/"><button className="btn btn-error text-white">Go Home</button></Link></span></h1>
         </div>
     );
 };
