981236dc6ea8be5d3208aabee328b8fbf3044af8
Shuvro0421 <adibarmanshuvro89@gmail.com>
2023-09-25 15:05:14 +0600

    pie chart done but needs to be polished

--- src/components/Statistics/Satistics.jsx
+++ src/components/Statistics/Satistics.jsx
@@ -1,51 +1,59 @@
 import React from 'react';
-import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, Cell, ResponsiveContainer, Legend } from 'recharts';
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const Satistics = () => {
+    const localStorageDonation = JSON.parse(localStorage.getItem('localStorageDonation')) || [];
+    const data = 12;
+    const totalDonation = localStorageDonation.length;
+    const YourDonation = ((100 * totalDonation) / data).toFixed(2);
+    const TotalDonation = (100 - YourDonation).toFixed(2);
 
-const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+    const datas = [
+        { name: 'Total Donation', value: parseFloat(TotalDonation) },
+        { name: 'Your Donation', value: parseFloat(YourDonation) }
+    ];
 
-  return (
-    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-      {`${(percent * 100).toFixed(0)}%`}
-    </text>
-  );
+    const COLORS = ['#FF444A', '#00C49F'];
+
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * (Math.PI / 180));
+        const y = cy + radius * Math.sin(-midAngle * (Math.PI / 180));
+        return (
+            <text x={x} y={y} fill="white" textAnchor="middle" dominantBaseline="central">
+                {`${(percent * 100).toFixed(2)}%`}
+            </text>
+        );
+    };
+
+    return (
+        <div>
+            <h1>Your Donation: {YourDonation}, Total Donation: {TotalDonation}</h1>
+            <h1>Donated in: {localStorageDonation.length}</h1>
+            <div className='border-2'>
+                <ResponsiveContainer width="100%" height={400}>
+                    <PieChart>
+                        <Pie
+                            data={datas}
+                            cx="50%"
+                            cy="50%"
+                            labelLine={false}
+                            label={renderCustomizedLabel}
+                            outerRadius={80}
+                            fill="#FF444A"
+                            dataKey="value"
+                            className='text-xs'
+                        >
+                            {datas.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                            ))}
+                        </Pie>
+                        <Legend />
+                    </PieChart>
+                </ResponsiveContainer>
+            </div>
+        </div>
+    );
 };
 
-const Statistics = ({ data, localStorageDonationLength }) => {
-  const remainingData = 12 - data;
-  const chartData = [
-    { name: 'Data', value: data },
-    { name: 'Remaining', value: remainingData },
-  ];
-
-  return (
-    <div>
-      <h1>Hello {localStorageDonationLength}</h1>
-      <ResponsiveContainer width="100%" height={400}>
-        <PieChart width={400} height={400}>
-          <Pie
-            data={chartData}
-            cx="50%"
-            cy="50%"
-            labelLine={false}
-            label={renderCustomizedLabel}
-            outerRadius={80}
-            fill="#8884d8"
-            dataKey="value"
-          >
-            {chartData.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-            ))}
-          </Pie>
-        </PieChart>
-      </ResponsiveContainer>
-    </div>
-  );
-};
-
-export default Statistics;
+export default Satistics;
